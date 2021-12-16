3d9afaa0518a83130e1a461a76741cb38e933c9b
Francisco Power <franciscopower66@gmail.com>
2021-12-16 17:10:42 +0000

    Config file, single file executable

--- battery_limiter_windows.py
+++ battery_limiter_windows.py
@@ -2,37 +2,51 @@ import psutil
 import schedule
 import time
 from win10toast import ToastNotifier
+import yaml
+import os
+import sys
 
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
 
-BATTERY_MAX_LIMIT=90
-BATTERY_MIN_LIMIT=90
+    return os.path.join(base_path, relative_path)
 
-
-def check_battery():
+def check_battery(bat_min_limit, bat_max_limit):
     battery = psutil.sensors_battery()
     bat_status = battery.power_plugged
     bat_percent = battery.percent
     
     #send notification if necessary
-    if bat_status and bat_percent > BATTERY_MAX_LIMIT:
+    if bat_status and bat_percent > bat_max_limit:
         toaster.show_toast("Unplug your computer!",
-                           f"Battery is more than {BATTERY_MAX_LIMIT}% full.\nUnplug it to keep your battery healthy.",
-                           icon_path="res\\Elegantthemes-Beautiful-Flat-Battery-full.ico"
+                           f"Battery is more than {bat_max_limit}% full.\nUnplug it to keep your battery healthy.",
+                           icon_path=resource_path("res\\Elegantthemes-Beautiful-Flat-Battery-full.ico")
                            )
                            
-    elif not bat_status and bat_percent < BATTERY_MIN_LIMIT:
+    elif not bat_status and bat_percent < bat_min_limit:
         toaster.show_toast("Low Battery",
-                           f"Battery level is below {BATTERY_MIN_LIMIT}%.\nIt's a good idea to plug your computer in.",
-                           icon_path="res\\Elegantthemes-Beautiful-Flat-Battery-low.ico"
+                           f"Battery level is below {bat_min_limit}%.\nIt's a good idea to plug your computer in.",
+                           icon_path=resource_path("res\\Elegantthemes-Beautiful-Flat-Battery-low.ico")
                            )
 
 
-toaster = ToastNotifier()
+def main():
+    #get settings
+    with open('C:\\Startup_scripts\\battery_monitor\\config.yaml') as config_file:
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
 
-# Check every 5 minutes
-# schedule.every(5).minutes.do(check_battery)
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
+    # # Check every 5 minutes
+    schedule.every(config['Notification_interval']).minutes.do(check_battery, config['Battery_min_limit'], config['Battery_max_limit'])
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
-check_battery() #Testing
+
+if __name__ == "__main__":
+    toaster = ToastNotifier()
+    main()
--- launcher.vbs
+++
@@ -1,2 +0,0 @@
-Set oShell = CreateObject ("WScript.Shell")
-oShell.run "pythonw battery_limiter_windows.py"
